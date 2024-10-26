7507a37052c7456ee4246de52a2c3a87d1fa6eff
YolcuM <maryam.pana@gmail.com>
2024-10-27 02:12:26 +0300

    0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Table from './components/Table';
+import data from './data';
+import Carousel  from './components/Carousel';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Logo from './components/Logo';
+import styles from './components/Navigation.module.css';
+import NotFound from './components/NotFound';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <header>
+            <nav>
+              <ul className={styles.navigation}>
+              <li><Link to='/game'>Тренажёр</Link></li>
+                <li><Logo /></li>
+                <li><Link to='/game'>Тренажёр</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <main>
+            <Routes>
+              <Route path='/' element={<Table/>} />
+              <Route path='/game' element={<Carousel cards ={data}/>} />
+              <Route path='*' element={<NotFound/>} />
+            </Routes>
+          </main>
+          <footer>
+
+          </footer>
+        </div>
+      </Router>
     </div>
   );
 }
